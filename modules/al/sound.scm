30c88a9a221c0af991e401d2e6f2dfa9fb245513
Alex Kost <alezost@gmail.com>
2026-02-12 13:21:36 +0300

    sound: Massive update
    1. Use custom arguments instead of amixer ones.
    
       Along with commit 579357409201f8cbc5868d6044843b8b846760f0, this
       allows to configure both, sound volume and display brightness,
       in a similar fashion.
    
    2. Do not export `call-amixer' and `parse-amixer-output'.  `get-sound'
       and `set-sound' should be the main entry points to control sound.
    
    3. Add and export `increase-volume' and `decrease-volume'.
    
    4. Remove `control' from <sound> record type.  When PulseAudio is used,
       amixer has only "Master" control anyway.

--- modules/al/sound.scm
+++ modules/al/sound.scm
@@ -1,6 +1,6 @@
 ;;; sound.scm --- Sound utilities
 
-;; Copyright © 2016, 2018 Alex Kost
+;; Copyright © 2016–2026 Alex Kost
 
 ;; Author: Alex Kost <alezost@gmail.com>
 ;; Created: 14 Jul 2016
@@ -26,54 +26,106 @@
 ;;; Code:
 
 (define-module (al sound)
+  #:use-module (ice-9 format)
   #:use-module (ice-9 regex)
-  #:use-module (srfi srfi-2)
   #:use-module (al processes)
   #:use-module (al records)
+  #:use-module (al let-macros)
   #:export (make-sound
             sound*
             sound?
-            sound-control
             sound-volume
             sound-muted?
             get-sound
             set-sound
-            call-amixer
-            parse-amixer-output))
+            increase-volume
+            decrease-volume))
 
 (define-record-type* <sound>
   make-sound sound*
   sound?
-  (control sound-control #f)    ; name of a simple control
-  (volume  sound-volume #f)     ; number from 0 to 100
-  (muted?  sound-muted? #f))    ; on or off
+  (volume sound-volume #f)      ; number from 0 to 100
+  (muted? sound-muted? #f))     ; on or off
+
+(define %amixer-control "Master")
 
 (define (call-amixer . args)
   "Call 'amixer' with ARGS and return its output."
+  ;; Use "-D hw:0" to avoid PulseAudio layer and connect directly to
+  ;; ALSA device.  Downsides: non-logarithmic percent scale, volume step
+  ;; is big (bigger than 1%), unmuting doesn't work.
+  ;;
+  ;; (apply system*-output "amixer" "-D" "hw:0" args)
   (apply system*-output "amixer" args))
 
 (define parse-amixer-output
-  (let ((control-rx  (make-regexp "Simple mixer control '([^']+)"))
-        (playback-rx (make-regexp
-                      "Playback [0-9]+ \\[([0-9]+)%\\].* \\[(on|off)\\]")))
+  (let ((rx (make-regexp
+             "Playback [0-9]+ \\[([0-9]+)%\\].* \\[(on|off)\\]")))
     (lambda (output)
       "Parse 'amixer' OUTPUT and return <sound> record.
 Return #f if OUTPUT cannot be parsed."
-      (and-let* ((control-match  (regexp-exec control-rx output))
-                 (playback-match (regexp-exec playback-rx output
-                                              (match:end control-match))))
+      (when-let ((match (regexp-exec rx output)))
         (sound*
-         #:control (match:substring control-match 1)
-         #:volume (string->number (match:substring playback-match 1))
-         #:muted? (string=? "off" (match:substring playback-match 2)))))))
+         #:volume (string->number (match:substring match 1))
+         #:muted? (string=? "off" (match:substring match 2)))))))
 
-(define (get-sound . args)
-  "Call 'amixer sget ARGS ...', parse its outputs and return <sound>
-structure."
-  (parse-amixer-output (apply call-amixer "sget" args)))
+(define (get-sound)
+  "Return <sound> structure for the current sound value."
+  (parse-amixer-output
+   (call-amixer "get" %amixer-control)))
 
-(define (set-sound . args)
-  "Call 'amixer sset ARGS ...'."
-  (apply system* "amixer" "sset" args))
+(define* (set-sound #:key volume mute #:rest args)
+  "Set sound according to VOLUME and MUTE arguments.
+
+VOLUME should be either:
+
+  - an integer from 0 to 100,
+
+  - a string in \"sN\" format, where \"N\" is an integer and \"s\" is an
+    optional sign i.e., \"s\" is either:
+
+    \"+\": to increase volume by N,
+    \"-\": to decrease volume by N,
+    empty string: to set volume to N.
+
+MUTE should be either #f, #t, or `toggle' symbol.
+
+Return <sound> structure for the current sound value."
+  (define (->volume-arg vol)
+    (cond
+     ((integer? vol)
+      (string-append (number->string vol) "%"))
+     ((string? vol)
+      (let ((first-char (car (string->list vol))))
+        (case first-char
+          ((#\+ #\-)
+           (format #f "~a%~c" (substring vol 1) first-char))
+          (else
+           (string-append vol "%")))))
+     (else #f)))
+
+  (define (->mute-arg mute)
+    (if mute
+      (if (eq? #t mute) "off" "toggle")
+      "on"))
+
+  (let* ((volume-arg (and (memq #:volume args)
+                          (->volume-arg volume)))
+         (mute-arg   (and (memq #:mute args)
+                          (->mute-arg mute)))
+         (args       (filter identity
+                             (list volume-arg mute-arg))))
+    (parse-amixer-output
+     (apply call-amixer "set" %amixer-control args))))
+
+(define (increase-volume percentage)
+  "Increase sound volume by PERCENTAGE.
+Return percentage of the current volume."
+  (set-sound #:volume (string-append "+" (number->string percentage))))
+
+(define (decrease-volume percentage)
+  "Decrease sound volume by PERCENTAGE.
+Return percentage of the current volume."
+  (set-sound #:volume (string-append "-" (number->string percentage))))
 
 ;;; sound.scm ends here
